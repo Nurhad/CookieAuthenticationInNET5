b7fc992f4106796b0f21ef17196ff6fa5dc6ea66
Nurhad <m.nurhadbarokah@gmail.com>
2021-05-19 12:37:04 +0700

    Add project files.

---
+++ CookieAuthenticationInNET5/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using CookieAuthenticationInNET5.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CookieAuthenticationInNET5.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserLogin([Bind] Users user)
+        {
+            var users = new Users();
+            var allUsers = users.GetUsers().FirstOrDefault();
+            if (users.GetUsers().Any(u => u.UserName == user.UserName))
+            {
+                var userClaims = new List<Claim>()
+                {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, "admin@test.com"),
+                 };
+
+                var grandmaIdentity = new ClaimsIdentity(userClaims, "User Identity");
+
+                var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity });
+                HttpContext.SignInAsync(userPrincipal);
+
+                return RedirectToAction("User", "Home");
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ CookieAuthenticationInNET5/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieAuthenticationInNET5.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+
+        public IEnumerable<Users> GetUsers()
+        {
+            return new List<Users>() { new Users { Id = 101, UserName = "admin", Name = "Administrtor", EmailId = "administrator@test.com", Password = "admin123" } };
+        }
+    }
+}
